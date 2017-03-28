16809291b5eb31c8f33ca8d52c55fda1f74b885c
PopRachsang <56030263@kmitl.ac.th>
2017-03-28 13:22:14 +0700

    Add files via upload

---
+++ LAB1/pop1.ino
@@ -0,0 +1,32 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(13, OUTPUT);
+}
+
+
+void loop() {
+  //L
+  short1();  long1();   short1();   short1();
+  //O
+  long1();  long1();   long1();  
+  //V
+  short1();   short1();  short1();  long1();
+  //e
+  short1();
+  delay(1400);
+}
+
+
+void short1() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(300);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(300);
+}
+
+void long1() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
